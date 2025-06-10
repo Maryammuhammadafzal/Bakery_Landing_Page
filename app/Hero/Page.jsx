98f18eb5ae13b710bb9bb62d0e48c1a89764db6e
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-10 16:08:24 +0500

    Adding Hero Section

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import CupcakeIcon from "../../public/cupcake-icon.png"
+import HeroImage from "../../public/images/Hero-image.png"
 import Image from "next/image";
 const HeroPage = () => {
   return (
-    <div className="flex h-auto md:h-screen justify-center items-center">
+    <div className="flex h-auto py-10 justify-center items-center">
       <div className="flex gap-5 justify-center items-center h-auto lg:w-[90%] w-[95%]">
-        <div className="left flex flex-col h-auto w-1/2 lg:pr-3 gap-4 justify-center">
-        <h1 className="flex flex-wrap text-6xl gap-2 items-center">
-                <span className="text-secondary">Baking Happiness,</span>
-                <span className="text-primary">One Crumb at a Time</span>
-                <span><Image width={50} height={50} src={CupcakeIcon} alt="icon" /></span>
+        <div className="left flex flex-col h-auto w-1/2 gap-4 justify-center">
+        <h1 className=" text-primary text-7xl gap-2 items-center">
+             <span className="text-primary"><span className="text-secondary">Baking Happiness,</span>One Crumb at a Time<Image width={50} height={50} className="inline mx-2" src={CupcakeIcon} alt="icon" /></span>
         </h1>
-        <p className="text-primary lato">
+        <p className="text-primary lato max-w-xl">
           <span className="text-secondary text-2xl">W</span>elcome to Crème Crust, your destination for handcrafted treats made with love and premium ingredients. Our passion for baking shines through every loaf, tart, and cookie we create. Discover the magic of fresh, from-scratch delights at our cozy bakery.
         </p>
         </div>
         <div className="right flex  h-auto w-1/2 items-center justify-center">
-        
+        <Image width={430} height={400} src={HeroImage} alt="icon" />
         </div>
       </div>
     </div>
